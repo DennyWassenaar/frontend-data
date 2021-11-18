5f025f5f815d6682bd70f6cb5a3d428e1fe9e67d
Denny Wassenaar <wassenaardenny@gmail.com>
2021-11-18 15:33:17 +0100

    Added comments

--- week-2/eindopdracht/js/script.js
+++ week-2/eindopdracht/js/script.js
@@ -7,7 +7,7 @@ var margin = { top: 10, right: 30, bottom: 50, left: 100 },
     height = 170 - margin.top - margin.bottom;
 
 
-// Prepare the circle pack space ---------------------------------------------------------------
+// Cirkel element selecteren en daar een SVG met waardes aan toevoegen
 var circle_g = d3.select("#circlepck").select('svg').attr('width', vWidth - margin.left).attr('height', vHeight + 50).select('g');
 
 // prepare line graph axes ------------------------------------------------------------------
@@ -15,7 +15,7 @@ const colorScheme = d3.scaleOrdinal(d3.schemeCategory20);
 
 all_data = {};
 
-// Get the data from our CSV file -----------------------------------------------------------
+// Data ophalen
 d3.json('data/genre.json', function (error, vCsvData) {
     if (error) throw error;
 
@@ -24,19 +24,20 @@ d3.json('data/genre.json', function (error, vCsvData) {
     drawViz(vData, circle_g);
 });
 
-// FUNCTION GENERATES DATA FOR CIRCLE PACK DIAGRAM --------------
+// functie voor het aanmaken van data voor de cirkeldiagram
 function genCirclePackData(data) {
 
     var vData = { name: "genre", children: data };
+
     return vData;
 }
 
-// FUNCTION GENERATES VIZ FOR CIRCLE PACKED DIAGRAM AND WORD CLOUD
+
 function drawViz(vData, space) {
-    // Declare d3 layout
+
     var vLayout = d3.pack().size([vWidth, vHeight]);
 
-    // Layout + Data
+
     var vRoot = d3.hierarchy(vData).sum(function (d) {
         var value = 0
         for (key in d) {
@@ -61,8 +62,7 @@ function drawViz(vData, space) {
             d3.select(".legendCells").selectAll(".cell").attr("opacity", "0.2");
         }
     })
-        .on("mouseover", function (thisElement, index) {
-            // grey out all legends                                   
+        .on("mouseover", function (thisElement, index) {                                 
             genre = '';
             for (key in thisElement.data) {
                 genre = key;
@@ -74,10 +74,9 @@ function drawViz(vData, space) {
                 }
                 return value;
             });
-            //showTooltip(thisElement.id,10,10);
         })
         .on("mouseout", function (thisElement, index) {
-            d3.select(".legendCells").selectAll(".cell").attr("opacity", "1"); // back to normal
+            d3.select(".legendCells").selectAll(".cell").attr("opacity", "1");
         });
 
     var labels = [];
@@ -94,7 +93,6 @@ function drawViz(vData, space) {
     colorScheme.domain(labels);
     console.log(labels);
 
-    // Draw on screen
     vSlices.attr('cx', function (d) { return d.x; })
         .attr('cy', function (d) { return d.y; })
         .attr('r', function (d) { return d.r; })
@@ -124,7 +122,7 @@ function drawViz(vData, space) {
         return color;
     });
 
-    // Encapsulate the word cloud functionality
+
 
 
     var fill = d3.scaleOrdinal(d3.schemeCategory10);
@@ -132,11 +130,9 @@ function drawViz(vData, space) {
     width = 400;
     height = 300;
 
-    // FUNCTION GENERATES WORD CLOUD OF MOVIES FOR SELECTED GENRE -----------------------
     function wordcloud(selectedGenre) {
         var myWords = [];
 
-        // append the svg object to the body of the page
         svgWC = d3.select("#wordcloud").append("svg")
             .attr("width", 1500)
             .attr("height", 700)
@@ -144,7 +140,7 @@ function drawViz(vData, space) {
             .attr("transform",
                 "translate(" + 20 + "," + 20 + ")");
 
-        // Parse the Data
+
         d3.csv("data/moviecloud.csv", function (data) {
             for (i in data) {
                 d = data[i];
@@ -153,25 +149,22 @@ function drawViz(vData, space) {
                 }
             }
 
-            // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
-            // Wordcloud features that are different from one word to the other must be here
             var layout = d3.layout.cloud()
                 .size([width, height])
                 .words(myWords.map(function (d) {
                     console.log(d['movie_title']);
                     return { text: d['movie_title'], size: parseFloat(d['imdb_score']), color: fill(d['movie_title']) };
                 }))
-                .padding(15)        //space between words
+                .padding(15)        
                 .rotate(function () { return ~~(Math.random() * 2) * 90; })
                 .fontSize(function (d) {
                     return (2 * d.size);
-                })                     // font size of words
+                })                   
                 .on("end", draw);
 
             layout.start();
 
-            // This function takes the output of 'layout' above and draw the words
-            // Wordcloud features that are THE SAME from one word to the other can be here
+        
             function draw(words) {
                 console.log(words);
                 svgWC
@@ -201,7 +194,7 @@ function drawViz(vData, space) {
         });
     }
 
-    // Legend -----------------------------------------------
+
     var g = space.append("g")
         .attr("class", "legendThreshold")
         .attr("transform", "translate(10,40)")
